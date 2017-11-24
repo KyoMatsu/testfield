9e428451095075537cd0c8fdf751e8c5bbe7189b
KyoMatsu <33883573+KyoMatsu@users.noreply.github.com>
2017-11-24 18:26:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('mysite-static-v3').then(function(cache) {
+      return cache.addAll([
+        'index.html'
+      ]);
+    })
+  );
+});
