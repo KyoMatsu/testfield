7b1d33b8ef31d746671f50fd8f1c477509d9ff41
KyoMatsu <33883573+KyoMatsu@users.noreply.github.com>
2018-01-30 14:08:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,8 +10,14 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュがあったのでそのレスポンスを返す
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
