c9d5e9f21786ea91a9ed0f1a123c9884fb2bc508
KyoMatsu <33883573+KyoMatsu@users.noreply.github.com>
2018-07-02 17:06:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,10 +7,13 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
   // インストール処理
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
 });
