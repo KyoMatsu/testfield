cc3180f27b4a571b0159a49ee9903de64a7a236a
KyoMatsu <33883573+KyoMatsu@users.noreply.github.com>
2019-01-19 23:54:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+navigator.serviceWorker.register('/testfield/sw.js', { scope: '/testfield/' }).then(function(registration) {
+    // 登録成功
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {
+    // 登録失敗 :(
+    console.log('ServiceWorker registration failed: ', err);
+});
